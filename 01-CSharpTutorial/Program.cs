d04d13be58208540fdd9834154b5d10f56ab7d07
Alma Olmos <almaolmos050495@gmail.com>
2024-03-09 18:52:41 -0600

    ejemplo uso de variables en otra clase

--- 01-CSharpTutorial/Program.cs
+++ 01-CSharpTutorial/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,21 +10,29 @@ namespace _01_CSharpTutorial
     internal class Program
     {
         public int Id { get; set; }
+
+        public class @ejemplo
+        {
+            public String Saludo { get; set; }
+        }
         static void Main(string[] args)
         {
-            Program program = new Program();
-            program.Id = 0;
+            bool MayorEdad = true;
             string message = "¡Hello World!";
+            ejemplo ej = new ejemplo();
+            ej.Saludo = "Saludo desde un objeto en otra clase";
             Console.WriteLine(message);
+            Console.WriteLine(ej.Saludo);
+
             Console.ReadLine();
+
         }
-    }
-    internal class Program2
-    {
-        public void metodo()
+        void metodo()
         {
-            Program program = new Program();
-            program.Id = 1;
+            this.Id=0;
         }
+        
+        
     }
+   
 }
